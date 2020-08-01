03237f045225555e8ece5839dac8402daea4834c
Sergii Artemenko <xtadex@gmail.com>
2020-08-01 23:59:03 +0200

    Squashed 'src/Xtate.Service.SmtpClient/' changes from 93f4d69..995d935
    995d935 Added symbols packages
    6fec5e9 Update Project Site Link
    a294b8e Added pushing to NuGet.org
    71d50e8 Reformat file header
    4947d7c Added Copyright headers
    6b998a3 Added LICENSE files and into Package description
    7250645 Update YML
    239fbd9 Added files azure-pipelines.yml
    81bf20f Switch to 0.0.0 version
    07a0397 Update version to 1.0.0-alpha
    f4b8a52 Update referenced packages
    daae551 Namespace refactoring
    82acbf7 Namespaces adjusted. Unit tests moved.
    f3955fa Moving from namespace TSSArt.StateMachine to Xtate
    
    git-subtree-dir: src/Xtate.Service.SmtpClient
    git-subtree-split: 995d9355eef8aab5da0b57d4b90dcf4d99bfd2de

--- src/Xtate.Service.SmtpClient/SmtpClientService.cs
+++ src/Xtate.Service.SmtpClient/SmtpClientService.cs
@@ -1,10 +1,29 @@
-﻿using System.Net.Mail;
+﻿#region Copyright © 2019-2020 Sergii Artemenko
+// 
+// This file is part of the Xtate project. <https://xtate.net/>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+#endregion
+
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TSSArt.StateMachine.Services
+namespace Xtate.Service
 {
-	[SimpleService("http://tssart.com/scxml/service/#SMTPClient", Alias = "smtp")]
+	[SimpleService("http://xtate.net/scxml/service/#SMTPClient", Alias = "smtp")]
 	public class SmtpClientService : SimpleServiceBase
 	{
 		public static readonly IServiceFactory Factory = SimpleServiceFactory<SmtpClientService>.Instance;
