10cad94564d1375ad6db65eb5abf145acccb36d0
Sergii Artemenko <xtadex@gmail.com>
2024-07-02 22:39:27 +0200

    Add Subtree Pull

---
+++ pull-subtrees.cmd
@@ -0,0 +1,4 @@
+git subtree pull --prefix src/Xtate.SharedContent         https://github.com/TSSArt/Xtate.SharedContent         main --squash
+git subtree pull --prefix src/Xtate.IoC                   https://github.com/TSSArt/Xtate.IoC                   main --squash
+git subtree pull --prefix src/Xtate.Core                  https://github.com/TSSArt/Xtate.Core                  main --squash
+git subtree pull --prefix src/Xtate.DataModel.EcmaScript  https://github.com/TSSArt/Xtate.DataModel.EcmaScript  main --squash
