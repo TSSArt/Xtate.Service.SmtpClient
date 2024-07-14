eba995639e0626acb3f188f2e952ce12d3af14eb
Sergii Artemenko <xtadex@gmail.com>
2024-07-14 14:04:15 +0200

    CMD updates

--- pull-subtrees.cmd
+++ pull-subtrees.cmd
@@ -1,5 +1,7 @@
-git subtree pull --prefix src/Xtate.SharedContent         https://github.com/TSSArt/Xtate.SharedContent         main --squash
-git subtree pull --prefix src/Xtate.IoC                   https://github.com/TSSArt/Xtate.IoC                   main --squash
-git subtree pull --prefix src/Xtate.Core                  https://github.com/TSSArt/Xtate.Core                  main --squash
-git subtree pull --prefix src/Xtate.DataModel.EcmaScript  https://github.com/TSSArt/Xtate.DataModel.EcmaScript  main --squash
-git subtree pull --prefix src/Xtate.Logger.Serilog        https://github.com/TSSArt/Xtate.Logger.Serilog        main --squash
+git subtree pull --prefix src/Xtate.SharedContent            https://github.com/TSSArt/Xtate.SharedContent            main --squash
+git subtree pull --prefix src/Xtate.IoC                      https://github.com/TSSArt/Xtate.IoC                      main --squash
+git subtree pull --prefix src/Xtate.Core                     https://github.com/TSSArt/Xtate.Core                     main --squash
+git subtree pull --prefix src/Xtate.DataModel.EcmaScript     https://github.com/TSSArt/Xtate.DataModel.EcmaScript     main --squash
+git subtree pull --prefix src/Xtate.Logger.Serilog           https://github.com/TSSArt/Xtate.Logger.Serilog           main --squash
+git subtree pull --prefix src/Xtate.IoProcessor.Http         https://github.com/TSSArt/Xtate.IoProcessor.Http         main --squash
+git subtree pull --prefix src/Xtate.IoProcessor.Http.Kestrel https://github.com/TSSArt/Xtate.IoProcessor.Http.Kestrel main --squash
--- push-subtrees.cmd
+++ push-subtrees.cmd
@@ -1,5 +1,7 @@
-git subtree push --prefix src/Xtate.SharedContent         https://github.com/TSSArt/Xtate.SharedContent         main
-git subtree push --prefix src/Xtate.IoC                   https://github.com/TSSArt/Xtate.IoC                   main
-git subtree push --prefix src/Xtate.Core                  https://github.com/TSSArt/Xtate.Core                  main
-git subtree push --prefix src/Xtate.DataModel.EcmaScript  https://github.com/TSSArt/Xtate.DataModel.EcmaScript  main
-git subtree push --prefix src/Xtate.Logger.Serilog        https://github.com/TSSArt/Xtate.Logger.Serilog        main
+git subtree push --prefix src/Xtate.SharedContent            https://github.com/TSSArt/Xtate.SharedContent            main
+git subtree push --prefix src/Xtate.IoC                      https://github.com/TSSArt/Xtate.IoC                      main
+git subtree push --prefix src/Xtate.Core                     https://github.com/TSSArt/Xtate.Core                     main
+git subtree push --prefix src/Xtate.DataModel.EcmaScript     https://github.com/TSSArt/Xtate.DataModel.EcmaScript     main
+git subtree push --prefix src/Xtate.Logger.Serilog           https://github.com/TSSArt/Xtate.Logger.Serilog           main
+git subtree push --prefix src/Xtate.IoProcessor.Http         https://github.com/TSSArt/Xtate.IoProcessor.Http         main
+git subtree push --prefix src/Xtate.IoProcessor.Http.Kestrel https://github.com/TSSArt/Xtate.IoProcessor.Http.Kestrel main
